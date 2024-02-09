b6d287b8365fa66a99b51fd787091ab0579b4a31
Yaron Peres <yaronp16@gmail.com>
2024-02-09 20:54:42 +0100

    change

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -6,21 +6,21 @@ function UserInput() {
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" min="0" step="1" />
+          <input type="number" required />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" min="0" step="1" />
+          <input type="number" required />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" min="0" step="1" />
+          <input type="number" required />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" min="0" step="1" />
+          <input type="number" required />
         </p>
       </div>
     </section>
