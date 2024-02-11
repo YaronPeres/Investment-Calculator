6485afe34cd4c95918581b90ba51255d54944b07
Yaron Peres <yaronp16@gmail.com>
2024-02-11 18:27:12 +0100

    done

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { calculateInvestmentResults, formatter } from "../util/investment.js";
-function UserInput() {
+function UserInput({ userInput, handleChange }) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+            value={userInput.initialInvestment.toString()}
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+            value={userInput.annualInvestment.toString()}
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+            value={userInput.expectedReturn.toString()}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) => handleChange("duration", event.target.value)}
+            value={userInput.duration.toString()}
+          />
         </p>
       </div>
     </section>
